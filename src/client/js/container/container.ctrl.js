9c265f5e07b2545bd8a3b5aad0c5168166a96c7f
Matthew Harwood <matthhar12@gmail.com>
2015-09-30 21:12:34 -0700

    added firebase

--- src/client/js/container/container.ctrl.js
+++ src/client/js/container/container.ctrl.js
@@ -1,13 +1,21 @@
 (function () {
   angular
       .module('divkick.container', [])
-      .controller('ContainerCtrl', ContainerCtrl);
-
+      .controller('ContainerCtrl', ContainerCtrl)
+      .$inject = ['$firebaseArray'];
   /* @ngInject */
-  function ContainerCtrl() {
+  function ContainerCtrl($firebaseArray ) {
     /* jshint validthis: true */
     var main = this;
+    var randomRoomId = Math.round(Math.random() * 100000000);
+    var ref = new Firebase("https://div-kick.firebaseio.com/room" + randomRoomId);
+    main.room = $firebaseArray(ref);
+    main.room.$add({
+      from: 'hey',
+      content: 'yah'
+    });
 
+    //
     main.title = 'div kick';
 
     activate();
@@ -19,3 +27,4 @@
     }
   }
 })();
+
